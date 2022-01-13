d5237e29af51fecfb8f26b30bdefe9f72b2e44a4
luiscarlossalvatierra <luiscarlos.salvatierra@elorrieta-errekamari.com>
2022-01-13 21:00:39 +0100

    json upload

--- src/json/JsonReader.java
+++ src/json/JsonReader.java
@@ -1,4 +1,5 @@
 package json;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,6 +12,7 @@ import java.text.SimpleDateFormat;
 import java.util.Iterator;
 import java.util.Map;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -21,42 +23,199 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import objetos.CalidadAireDiario;
 import objetos.CalidadAireHorario;
+import objetos.CalidadAireIndice;
+import objetos.EspaciosNaturales;
 import objetos.Estacion;
-
-
+import objetos.Municipio;
 
 public class JsonReader {
 	private static final String url = "https://opendata.euskadi.eus/contenidos/ds_informes_estudios/calidad_aire_2021/es_def/adjuntos/index.json";
-	
+
 	public static void main(String args[]) {
 		HttpCert.validCert();
-		uploadEstaciones();
-		//refreshHorarios();
+		// municipio
+		//uploadMunicipio();
+		// espacios naturales
+		//uploadEspaciosNaturales();
+		// estaciones
+		//uploadEstaciones();
+
+		refreshHorarios();
 	}
-	
-	public static void uploadEstaciones() {
-		String urlEstacion = "https://opendata.euskadi.eus/contenidos/ds_informes_estudios/calidad_aire_2020/es_def/adjuntos/estaciones.json";
-		
-		JsonObject objectEstacion = readJsonCrashedFromUrl(urlEstacion);
-		
+
+	public static void uploadMunicipio() {
+		String urlMunicipio = "https://drive.google.com/uc?id=1syFXxNOiNZQ-Zf_BNk0uTDgeZxV4CaB0&export=download";
+
+		JsonObject objectEstacion = readJsonCrashedFromUrl(urlMunicipio);
+
 		JsonArray array = (JsonArray) objectEstacion.get("jsonData");
 		Iterator<JsonElement> iter = array.iterator();
-		
-		Estacion estacion = new Estacion();
-		int count = 0;
+
+		Municipio municipio = new Municipio();
+		System.out.println("Municipio");
 		while (iter.hasNext()) {
-			
 			JsonElement entrada = iter.next();
 			JsonObject objeto = entrada.getAsJsonObject();
 			Iterator<Map.Entry<String, JsonElement>> iter2 = objeto.entrySet().iterator();
 			Iterator<Map.Entry<String, JsonElement>> iter3 = objeto.entrySet().iterator();
-			
-			while(iter2.hasNext()) {
+
+			while (iter2.hasNext()) {
 				String key = iter2.next().getKey().toString();
 				String value = iter3.next().getValue().getAsString();
-				
-				switch(key) {
+
+				switch (key) {
+				case "documentName":
+					municipio.setNombre(value);
+					break;
+				case "turismDescription":
+					municipio.setDescripcion(value);
+					break;
+				case "locality":
+					municipio.setLocalidad(value);
+					break;
+				case "marks":
+					municipio.setMarca(value);
+					break;
+				case "territory":
+					municipio.setTerritorio(value);
+					break;
+				case "templateType":
+					municipio.setTipo(value);
+					break;
+				case "territorycode":
+					municipio.setTerritoryCode(value);
+					break;
+				}
+
+				if (!iter2.hasNext()) {
+					Transaction tx;
+					SessionFactory sesion = HibernateUtil.getSessionFactory();
+					Session s = sesion.openSession();
+					tx = s.beginTransaction();
+
+					// Guardar objeto en la base de datos
+					s.save(municipio);
+					// Actualizar información en la base de datos
+					tx.commit();
+
+					municipio = new Municipio();
+				}
+			}
+		}
+	}
+
+	public static void uploadEspaciosNaturales() {
+		String urlEspacioNatural = "https://drive.google.com/uc?id=1oi-R1Y52sPGlNxLjWg2WgHZRQ5kwZQw8&export=download";
+
+		JsonObject objectEstacion = readJsonCrashedFromUrl(urlEspacioNatural);
+
+		JsonArray array = (JsonArray) objectEstacion.get("jsonData");
+		Iterator<JsonElement> iter = array.iterator();
+
+		String idMunicipio = "";
+		EspaciosNaturales espacio = new EspaciosNaturales();
+		while (iter.hasNext()) {
+			JsonElement entrada = iter.next();
+			JsonObject objeto = entrada.getAsJsonObject();
+			Iterator<Map.Entry<String, JsonElement>> iter2 = objeto.entrySet().iterator();
+			Iterator<Map.Entry<String, JsonElement>> iter3 = objeto.entrySet().iterator();
+
+			while (iter2.hasNext()) {
+				String key = iter2.next().getKey().toString();
+				String value = iter3.next().getValue().getAsString();
+
+				switch (key) {
+				case "documentName":
+					espacio.setNombre(value);
+					break;
+				case "turismDescription":
+					espacio.setDescripcion(value);
+					break;
+				case "locality":
+					espacio.setLocalidad(value);
+					break;
+				case "municipality":
+					idMunicipio = value;
+					break;
+				case "territory":
+					espacio.setTerritorio(value);
+					break;
+				case "marks":
+					espacio.setMarca(value);
+					break;
+				case "templateType":
+					espacio.setTipo(value);
+					break;
+				case "natureType":
+					espacio.setNaturaleza(value);
+					break;
+				}
+
+				if (!iter2.hasNext()) {
+					Transaction tx;
+					SessionFactory sesion = HibernateUtil.getSessionFactory();
+					Session s = sesion.openSession();
+					tx = s.beginTransaction();
+
+					String operator = "/";
+					if (idMunicipio.contains("*")) {
+						operator = "\\*";
+					}
+
+					String result = "";
+
+					if (idMunicipio.contains("San Sebastián")) {
+						result = "San Sebastián";
+					} else {
+						result = (idMunicipio.split(operator))[0];
+					}
+
+					// Obtenemos el objeto para idmunicipio
+					String hql = "From Municipio where nombre = '" + result + "'";
+					Query q = s.createQuery(hql);
+
+					Municipio municipio = (Municipio) q.uniqueResult();
+
+					if (municipio != null) {
+						espacio.setMunicipio(municipio);
+
+						// Guardar objeto en la base de datos
+						s.save(espacio);
+						// Actualizar información en la base de datos
+						tx.commit();
+
+						espacio = new EspaciosNaturales();
+						idMunicipio = "";
+					}
+				}
+			}
+		}
+	}
+
+	public static void uploadEstaciones() {
+		String urlEstacion = "https://opendata.euskadi.eus/contenidos/ds_informes_estudios/calidad_aire_2020/es_def/adjuntos/estaciones.json";
+
+		JsonObject objectEstacion = readJsonCrashedFromUrl(urlEstacion);
+
+		JsonArray array = (JsonArray) objectEstacion.get("jsonData");
+		Iterator<JsonElement> iter = array.iterator();
+
+		String idMunicipio = "";
+		Estacion estacion = new Estacion();
+		while (iter.hasNext()) {
+
+			JsonElement entrada = iter.next();
+			JsonObject objeto = entrada.getAsJsonObject();
+			Iterator<Map.Entry<String, JsonElement>> iter2 = objeto.entrySet().iterator();
+			Iterator<Map.Entry<String, JsonElement>> iter3 = objeto.entrySet().iterator();
+
+			while (iter2.hasNext()) {
+				String key = iter2.next().getKey().toString();
+				String value = iter3.next().getValue().getAsString();
+
+				switch (key) {
 				case "Name":
 					estacion.setNombre(value);
 					break;
@@ -64,7 +223,7 @@ public class JsonReader {
 					estacion.setProvincia(value);
 					break;
 				case "Town":
-					estacion.setMunicipio(value);
+					idMunicipio = value;
 					break;
 				case "Address":
 					estacion.setDireccion(value);
@@ -76,145 +235,352 @@ public class JsonReader {
 					estacion.setLongitud(value);
 					break;
 				}
+
+				if (!iter2.hasNext()) {
+					Transaction tx;
+					SessionFactory sesion = HibernateUtil.getSessionFactory();
+					Session s = sesion.openSession();
+					tx = s.beginTransaction();
+
+					String operator = "/";
+					if (idMunicipio.contains("*")) {
+						operator = "\\*";
+					}
+
+					String result = "";
+
+					if (idMunicipio.contains("San Sebastián")) {
+						result = "San Sebastián";
+					} else if(idMunicipio.contains("Agurain")) {
+						result = "Salvatierra/Agurain";
+					} else {
+						result = (idMunicipio.split(operator))[0];
+					}
+
+					// Obtenemos el objeto para idmunicipio
+					String hql = "From Municipio where nombre = '" + result + "'";
+					Query q = s.createQuery(hql);
+
+					Municipio municipio = (Municipio) q.uniqueResult();
+
+					if (municipio != null) {
+						estacion.setMunicipio(municipio);
+
+						// Guardar objeto en la base de datos
+						s.save(estacion);
+						// Actualizar información en la base de datos
+						tx.commit();
+
+						estacion = new Estacion();
+						idMunicipio = "";
+					}
+				}
+			}
+		}
+	}
+
+	public static void refreshHorarios() {
+		JsonObject mainHorarios = readJsonFromUrl(url);
+
+		JsonArray array = (JsonArray) mainHorarios.get("aggregated");
+		Iterator<JsonElement> iter = array.iterator();
+
+		int count = 0;
+
+		while (iter.hasNext()) {
+			JsonElement entrada = iter.next();
+			JsonObject objeto = entrada.getAsJsonObject();
+			Iterator<Map.Entry<String, JsonElement>> iter2 = objeto.entrySet().iterator();
+
+			iter2.next();
+
+			String nameEstacion = iter2.next().getValue().getAsJsonPrimitive().getAsString();
+
+			String jsonStringUrl = iter2.next().getValue().getAsJsonPrimitive().getAsString();
+
+			switch (count) {
+			case 0:
+				// horarios
+
+				//datosHorariosGenerator(nameEstacion, jsonStringUrl);
+
+				count++;
+				break;
+			case 1:
+				// diarios
+				
+				datosDiarioGenerator(nameEstacion, jsonStringUrl);
+
+				count++;
+				break;
+			case 2:
+				// indice
+
+				//datosIndiceGenerator(nameEstacion, jsonStringUrl);
+
+				count = 0;
+				break;
+			}
+
+		}
+	}
+
+	private static void datosDiarioGenerator(String name, String urlEstacion) {
+		CalidadAireDiario aire = new CalidadAireDiario();
+
+		JsonObject datos = readJsonCrashedFromUrl(urlEstacion);
+
+		if (datos == null) {
+			return;
+		}
+
+		JsonArray array = (JsonArray) datos.get("jsonData");
+		Iterator<JsonElement> iter = array.iterator();
+
+		while (iter.hasNext()) {
+			JsonElement entrada = iter.next();
+			JsonObject objeto = entrada.getAsJsonObject();
+			Iterator<Map.Entry<String, JsonElement>> iter2 = objeto.entrySet().iterator();
+			Iterator<Map.Entry<String, JsonElement>> iter3 = objeto.entrySet().iterator();
+
+			while (iter2.hasNext()) {
+				String key = iter2.next().getKey().toString();
+				String value = iter3.next().getValue().getAsString();
+			
+				switch(key) {
+				case "Date":
+					try {
+						aire.setFecha(new SimpleDateFormat("dd/MM/yyyy").parse(value));
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+				case "NOgm3":
+					aire.setNogm3(value);
+					break;
+				case "NO2gm3":
+					aire.setNo2gm3(value);
+					break;
+				case "NOXgm3":
+					aire.setNoxgm3(value);
+					break;
+				case "PM10gm3":
+					aire.setPm10gm3(value);
+					break;
+				case "PM25gm3":
+					aire.setPm25gm3(value);
+					break;
+				}
 				
 				if(!iter2.hasNext()) {
 					Transaction tx;
 					SessionFactory sesion = HibernateUtil.getSessionFactory();
 					Session s = sesion.openSession();
 					tx = s.beginTransaction();
-
-					// Guardar objeto en la base de datos
-					s.save(estacion);
-					// Actualizar información en la base de datos
-					tx.commit();
 					
-					estacion = new Estacion();
-					count++;
-					System.out.println(count);
+					String response = name;
+					if (name.contains("_")) {
+						response = response.replace("_", " ");
+					}
+
+					String hql = "From Estacion where nombre LIKE '%" + (response.split("/"))[0] + "%'";
+					Query q = s.createQuery(hql);
+					q.setMaxResults(1);
+					Estacion estacion = (Estacion) q.uniqueResult();
+
+					if (estacion != null) {
+						aire.setEstacion(estacion);
+						// Guardar objeto en la base de datos
+						s.save(aire);
+						// Actualizar información en la base de datos
+						tx.commit();
+						aire = new CalidadAireDiario();
+					}
+					
+					return;
 				}
 			}
 		}
 	}
-	
-	public static void refreshHorarios() {
-		JsonObject mainHorarios = readJsonFromUrl(url);
 
-		JsonArray array = (JsonArray) mainHorarios.get("aggregated");
-		Iterator<JsonElement> iter = array.iterator();
-		
-		int count = 0;
-		
-		while (iter.hasNext()) {
-			JsonElement entrada = iter.next();
-			JsonObject objeto = entrada.getAsJsonObject();
-			Iterator<Map.Entry<String, JsonElement>> iter2 = objeto.entrySet().iterator();
-			
-			iter2.next();
-			
-			String nameEstacion = iter2.next().getValue().getAsJsonPrimitive().getAsString();
-			
-			String jsonStringUrl = iter2.next().getValue().getAsJsonPrimitive().getAsString();
-			
-			switch(count) {
-			case 0:
-				//horarios
-				
-				datosHorariosGenerator(nameEstacion, jsonStringUrl);
-				
-				count++;
-				break;
-			case 1:
-				//diarios
-				
-				
-				
-				count++;
-				break;
-			case 2:
-				//indice
-				
-				
-				
-				count = 0;
-				break;
-			}
-			
-		}
-	}
+	private static void datosIndiceGenerator(String name, String urlEstacion) {
+		CalidadAireIndice aire = new CalidadAireIndice();
 
-	private static void datosHorariosGenerator(String name, String urlEstacion) {
-		CalidadAireHorario aire = new CalidadAireHorario();
-		
 		JsonObject datos = readJsonCrashedFromUrl(urlEstacion);
-		
+
+		if (datos == null) {
+			return;
+		}
+
 		JsonArray array = (JsonArray) datos.get("jsonData");
 		Iterator<JsonElement> iter = array.iterator();
-		
+
 		int count = 0;
-		
 		while (iter.hasNext()) {
 			JsonElement entrada = iter.next();
 			JsonObject objeto = entrada.getAsJsonObject();
 			Iterator<Map.Entry<String, JsonElement>> iter2 = objeto.entrySet().iterator();
 			Iterator<Map.Entry<String, JsonElement>> iter3 = objeto.entrySet().iterator();
-			
-			while(iter2.hasNext()) {
+
+			while (iter2.hasNext()) {
+				String key = iter2.next().getKey().toString();
+				String value = iter3.next().getValue().getAsString();
+
+				switch (key) {
+				case "Date":
+					try {
+						aire.setFecha(new SimpleDateFormat("dd/MM/yyyy").parse(value));
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+				case "HourGMT":
+					aire.setHora(value);
+					break;
+				case "NO2ICA":
+					aire.setNo2ica(value);
+					break;
+				case "NOXgm3":
+					aire.setNoxgm3(value);
+					break;
+				case "PM10ICA":
+					aire.setPm10ica(value);
+					break;
+				case "PM25ICA":
+					aire.setPm25ica(value);
+					break;
+				case "SO2ICA":
+					aire.setSo2ica(value);
+					break;
+				case "ICAEstacion":
+					aire.setIcaestacion(value);
+					break;
+				}
+
+				if (!iter2.hasNext()) {
+					Transaction tx;
+					SessionFactory sesion = HibernateUtil.getSessionFactory();
+					Session s = sesion.openSession();
+					tx = s.beginTransaction();
+
+					String response = name;
+					if (name.contains("_")) {
+						response = response.replace("_", " ");
+					}
+
+					String hql = "From Estacion where nombre = '" + response + "'";
+					Query q = s.createQuery(hql);
+					q.setMaxResults(1);
+					Estacion estacion = (Estacion) q.uniqueResult();
+
+					if (estacion != null) {
+						aire.setEstacion(estacion);
+						// Guardar objeto en la base de datos
+						s.save(aire);
+						// Actualizar información en la base de datos
+						tx.commit();
+						aire = new CalidadAireIndice();
+					}
+					count++;
+				}
+
+				if (count == 24) {
+					return;
+				}
+			}
+		}
+	}
+
+	private static void datosHorariosGenerator(String name, String urlEstacion) {
+		CalidadAireHorario aire = new CalidadAireHorario();
+
+		JsonObject datos = readJsonCrashedFromUrl(urlEstacion);
+
+		if (datos == null) {
+			return;
+		}
+
+		JsonArray array = (JsonArray) datos.get("jsonData");
+		Iterator<JsonElement> iter = array.iterator();
+
+		int count = 0;
+
+		while (iter.hasNext()) {
+			JsonElement entrada = iter.next();
+			JsonObject objeto = entrada.getAsJsonObject();
+			Iterator<Map.Entry<String, JsonElement>> iter2 = objeto.entrySet().iterator();
+			Iterator<Map.Entry<String, JsonElement>> iter3 = objeto.entrySet().iterator();
+
+			while (iter2.hasNext()) {
 				String value = iter2.next().getKey().toString();
 				String valueData = iter3.next().getValue().getAsString();
-				
-				//Para añadir los datos al objeto
-				if(value.contains("Date")) {
-					
+
+				// Para añadir los datos al objeto
+				if (value.contains("Date")) {
+
 					try {
 						aire.setFecha(new SimpleDateFormat("dd/MM/yyyy").parse(valueData));
 					} catch (ParseException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					
-				} else if(value.equals("HourGMT")) {
+
+				} else if (value.equals("HourGMT")) {
 					aire.setHora(valueData);
-				} else if(value.equals("COmgm3")) {
+				} else if (value.equals("COmgm3")) {
 					aire.setComgm3(valueData);
-				} else if(value.equals("NOgm3")) {
+				} else if (value.equals("NOgm3")) {
 					aire.setNogm3(valueData);
-				} else if(value.equals("NO2gm3")) {
+				} else if (value.equals("NO2gm3")) {
 					aire.setNo2gm3(valueData);
-				} else if(value.equals("NOXgm3")) {
+				} else if (value.equals("NOXgm3")) {
 					aire.setNoxgm3(valueData);
-				} else if(value.equals("PM10gm3")) {
+				} else if (value.equals("PM10gm3")) {
 					aire.setPm10gm3(valueData);
-				} else if(value.equals("PM25gm3")) {
+				} else if (value.equals("PM25gm3")) {
 					aire.setPm25gm3(valueData);
-				} else if(value.equals("SO2gm3")) {
+				} else if (value.equals("SO2gm3")) {
 					aire.setSo2gm3(valueData);
 				}
-				
-				if(!iter2.hasNext()) {
-					
-					//aire.setEstacion(new Estacion());
-					
-					
-					SessionFactory factory = new Configuration().configure("euskalmet.hibernate.cfg.xml").addAnnotatedClass(CalidadAireHorario.class)
-			                .buildSessionFactory();
 
-			        // create session
-			        Session session = factory.getCurrentSession();
-					
-					session.save(aire);
-					session.getTransaction().commit();
-					
+				if (!iter2.hasNext()) {
+
+					Transaction tx;
+					SessionFactory sesion = HibernateUtil.getSessionFactory();
+					Session s = sesion.openSession();
+					tx = s.beginTransaction();
+
+					String response = name;
+					if (name.contains("_")) {
+						response = response.replace("_", " ");
+					}
+
+					String hql = "From Estacion where nombre = '" + response + "'";
+					Query q = s.createQuery(hql);
+					q.setMaxResults(1);
+					Estacion estacion = (Estacion) q.uniqueResult();
+
+					if (estacion != null) {
+						aire.setEstacion(estacion);
+						// Guardar objeto en la base de datos
+						s.save(aire);
+						// Actualizar información en la base de datos
+						tx.commit();
+						aire = new CalidadAireHorario();
+					}
 				}
 			}
-			
+
 			count++;
-			
-			if(count == 24) {
+
+			if (count == 24) {
 				return;
 			}
 		}
 	}
-	
+
 	private static JsonObject readJsonFromUrl(String url) {
 		InputStream is = null;
 		try {
@@ -249,20 +615,19 @@ public class JsonReader {
 		try {
 			is = new URL(url).openStream();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return null;
 		}
 		try {
 			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
 			String jsonText = null;
 			try {
 				jsonText = readAll(rd);
-				
-				//Estandarizamos el json para que pueda ser legible para los metodos
+
+				// Estandarizamos el json para que pueda ser legible para los metodos
 				jsonText = jsonText.replace("jsonCallback([", "");
 				jsonText = jsonText.replace("]);", "");
-				jsonText = "{ \"jsonData\": [" + jsonText + "]}";			
-				
+				jsonText = "{ \"jsonData\": [" + jsonText + "]}";
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -278,7 +643,7 @@ public class JsonReader {
 			}
 		}
 	}
-	
+
 	private static String readAll(Reader rd) throws IOException {
 		StringBuilder sb = new StringBuilder();
 		int cp;
