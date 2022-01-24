2a1a7cbf60acd48ae10b506f48d7bff739819583
jomas94 <jomariojsa@gmail.com>
2022-01-24 17:46:27 +0100

    image login

--- src/vista/VentanaLogin.java
+++ src/vista/VentanaLogin.java
@@ -15,8 +15,13 @@ import javax.swing.JSpinner;
 import javax.swing.JPasswordField;
 import javax.swing.JToggleButton;
 import javax.swing.JCheckBox;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
 
@@ -48,37 +53,32 @@ public class VentanaLogin extends JFrame {
 	public VentanaLogin() {
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 250, 280);
+		setBounds(100, 100, 310, 359);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Inicio de sesi\u00F3n");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(10, 11, 214, 14);
-		contentPane.add(lblNewLabel);
-
 		JLabel lblNewLabel_1 = new JLabel("Usuario");
-		lblNewLabel_1.setBounds(10, 36, 86, 14);
+		lblNewLabel_1.setBounds(52, 107, 86, 14);
 		contentPane.add(lblNewLabel_1);
 
 		TxtUsuario = new JTextField();
-		TxtUsuario.setBounds(10, 61, 214, 20);
+		TxtUsuario.setBounds(52, 132, 214, 20);
 		contentPane.add(TxtUsuario);
 		TxtUsuario.setColumns(10);
 
 		JLabel lblNewLabel_2 = new JLabel("Contrase\u00F1a");
-		lblNewLabel_2.setBounds(10, 92, 86, 14);
+		lblNewLabel_2.setBounds(52, 163, 86, 14);
 		contentPane.add(lblNewLabel_2);
 
 		passwordField = new JPasswordField();
-		passwordField.setBounds(10, 117, 214, 20);
+		passwordField.setBounds(52, 188, 214, 20);
 		contentPane.add(passwordField);
 
 		JCheckBox ChechRecordar = new JCheckBox("Recordarme");
 		ChechRecordar.setBounds(10, 144, 97, 23);
-		contentPane.add(ChechRecordar);
+		//contentPane.add(ChechRecordar);
 
 		JButton BtnIniciarSesion = new JButton("Iniciar sesion");
 		BtnIniciarSesion.addActionListener(new ActionListener() {
@@ -87,7 +87,7 @@ public class VentanaLogin extends JFrame {
 				dispose();
 			}
 		});
-		BtnIniciarSesion.setBounds(60, 174, 110, 23);
+		BtnIniciarSesion.setBounds(103, 219, 110, 23);
 		contentPane.add(BtnIniciarSesion);
 
 		JButton BtnRegistro = new JButton("Registrarse");
@@ -98,7 +98,23 @@ public class VentanaLogin extends JFrame {
 				dispose();
 			}
 		});
-		BtnRegistro.setBounds(60, 208, 110, 23);
+		BtnRegistro.setBounds(103, 253, 110, 23);
 		contentPane.add(BtnRegistro);
+		
+		
+		BufferedImage myPicture=null;
+		///Euskalmet/src/euskalmet.png
+			try {
+				myPicture = ImageIO.read(new File("./src/euskalmet.png"));
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	
+		
+		JLabel lblImage = new JLabel(new ImageIcon(myPicture));
+		lblImage.setBounds(111, 11, 86, 85);
+		contentPane.add(lblImage);
+	
 	}
 }
